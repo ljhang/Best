34eb1bddb4ad3ebc9d855e03d1d5fcbfb3086c3c
李建航 <1272927624@qq.com>
2018-09-21 17:26:02 +0800

    add Map Key

--- utils/util.js
+++ utils/util.js
@@ -22,8 +22,14 @@ var getParameter = function(page) {
 
 const domain = "http://paper-cdn.2q10.com"
 
+const app_secret = "4702b42e60d35f734e63c949a7678d83"
+
+const tx_map_key = "EOZBZ-UXK3V-FNOPW-UJ7P4-D7NT3-27FUU"
+
 module.exports = {
   formatTime: formatTime,
   getParameter: getParameter,
-  domain: domain
+  domain: domain,
+  app_secret: app_secret,
+  tx_map_key: tx_map_key
 }
